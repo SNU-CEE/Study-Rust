2437b44caf5290bcf36d83a7dab05507caa97c08
haksoon <bafflesoon@snu.ac.kr>
2023-01-11 17:32:17 +0900

    Study Rust 7

---
+++ my-project/target/debug/deps/my_project-d5871af3dbfb8b18.d
@@ -0,0 +1,5 @@
+c:\Users\User\Study Rust\my-project\target\debug\deps\my_project-d5871af3dbfb8b18.rmeta: src\main.rs
+
+c:\Users\User\Study Rust\my-project\target\debug\deps\my_project-d5871af3dbfb8b18.d: src\main.rs
+
+src\main.rs:
---
+++ restaurant/target/debug/deps/restaurant-2c4d59c213fd3db3.d
@@ -0,0 +1,5 @@
+c:\Users\User\Study Rust\restaurant\target\debug\deps\restaurant-2c4d59c213fd3db3.rmeta: src\lib.rs
+
+c:\Users\User\Study Rust\restaurant\target\debug\deps\restaurant-2c4d59c213fd3db3.d: src\lib.rs
+
+src\lib.rs:
